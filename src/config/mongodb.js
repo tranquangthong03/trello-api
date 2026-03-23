87856f841f871f46e531a0ad01c595f5dd57d261
Tran Quang Thong <tranquangthong03012005@gmail.com>
2026-03-23 10:26:19 +0700

    Setup and connect to Mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,37 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+// quangthongdev
+// password: vcqaPe8Z614fgPwU
+
+const MONGODB_URI = 'mongodb+srv://quangthongdev:vcqaPe8Z614fgPwU@cluster0.ghftqag.mongodb.net/?appName=Cluster0'
+
+const DATABASE_NAME = 'trello-database-quangthong'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGODB_URI,
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true
+    },
+    tls: true,
+    tlsAllowInvalidCertificates: true // thêm dòng này
+  }
+)
+// Kết nối tới database
+export const CONNECT_DB = async () => {
+  // Gọi kết nối tới mongodb atlas với uri đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect()
+
+  // Kết nối thành công thì lấy ra database có DATABASE_NAME và gán ngược lại nso vào biến trelloDatabaseInstance
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
+export const CLOSE_DB = () => {
+  return mongoClientInstance.close()
+}
