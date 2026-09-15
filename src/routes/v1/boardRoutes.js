d283cd2be7858b575bd6c3c8da6c6e505eb10a96
quangthong <tranquangthong03012005@gmail.com>
2026-09-15 22:07:54 +0700

    feat(board): add routes and validation for board

---
+++ src/routes/v1/boardRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardValidation } from '~/validations/boardValidation'
+const Router = express.Router()
+
+Router.route('/')
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({ Message: 'Note: API get list board' })
+  })
+  .post(boardValidation.createNew)
+
+export const boardRoutes = Router
