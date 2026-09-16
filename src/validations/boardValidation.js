47b2950cd0b331a849a1ac7759d94972c33533ce
quangthong <tranquangthong03012005@gmail.com>
2026-09-16 17:39:05 +0700

    feat: write middleware error handling

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes'
 import Joi from 'joi'
-
+import ApiError from '~/utils/ApiError'
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     title: Joi.string().min(3).max(50).trim().strict(),
@@ -12,11 +12,10 @@ const createNew = async (req, res, next) => {
     // Validate dữ liệu xong sẽ được chạy qua tầng controller
     next()
   } catch (error) {
-    console.log(error)
-    console.log(new Error)
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    // Nếu có lỗi thì thực hiện xử lý lỗi tập trung ở middleware error handling
+    // Hàm next nhận vào 1 tham số là error
+    // Class ApiError nhận vào 2 tham số là statuscode và error message
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, error.message))
   }
 }
 
