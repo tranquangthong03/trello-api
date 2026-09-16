fc09707017a8f78be0a92234e3bf78aa9ab10287
quangthong <tranquangthong03012005@gmail.com>
2026-09-16 18:34:22 +0700

    mode dev and prod

--- src/config/environment.js
+++ src/config/environment.js
@@ -4,5 +4,6 @@ export const env = {
   MONGODB_URI: process.env.MONGODB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
   APP_PORT: process.env.APP_PORT,
+  BUILD_MODE: process.env.APP_PORT,
   APP_HOST: process.env.APP_HOST
 }
