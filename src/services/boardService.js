86b15a0562de86a14681e2aecfd3ca2f2265d133
quangthong <tranquangthong03012005@gmail.com>
2026-09-20 22:12:41 +0700

    create board api

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,13 +1,18 @@
 import { slugify } from '~/utils/formartters'
-
+import { boardModel } from '~/models/boardModel'
+import { GET_DB } from '~/config/mongodb'
 const createNew = async (reqBody) => {
   const newBoard = {
     ...reqBody,
     slug: slugify(reqBody.title)
   }
-
-  return newBoard
+  const createdBoard = await boardModel.createNew(newBoard)
+  console.log(createdBoard)
+  const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+  console.log(getNewBoard)
+  return getNewBoard
 }
+
 
 export const boardService = {
   createNew
